0f63f3d8ee5866bf358cace646315f9e0cc2f5fc
Sharabati Bose <sharabati.bose2006@gmail.com>
2025-10-26 01:09:36 +0530

    Implement 1-second countdown in timefunc.py
    Added sleep function to create a 1-second countdown gap.

--- timefunc.py
+++ timefunc.py
@@ -1,9 +1,10 @@
 import time
 my_time=int(input("enter time in seconds:"))
-
+#to countdown an input amount of seconds.
 for x in range (my_time, 0, -1):
     seconds= x%60
     minutes= (x//60)%60
     hours= x//3600
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
+#countdown at 1sec gaps 
     time.sleep(1)
